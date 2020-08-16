92de9295354ceebaede82e5f7075fc458b3cee93
Christina Celestino <christinacx7@gmail.com>
2020-08-15 21:11:28 -0500

    The user now gets the option to solve a an equation where they are trying to find P(A or B), found out the reason prints were coming out surrounded with '' was because vscode was using python2 to run instead of python3.  Changed to python3, added shebang line and adjusted syntax to better fit python3 but now code wont run after the user makes a selection.

--- statisticsHelp.py
+++ statisticsHelp.py
@@ -1,17 +1,23 @@
+#!/usr/bin/python3
+
 import math
 
-#Statistics helper will be able to solve statistics problems
+#  Statistics helper will be able to solve statistics problems by
+#calculating the problems for the user
 
 print("\nStatistics Helper!")
 
 #ask the user what equation they are trying to solve
 print("What kind of equation are you trying to solve?")
-print("1. Chebyshev's inequality equation\n2. IQR with given Q's")
+print("1. Chebyshev's inequality equation")
+print("2. IQR with given Q's")
+print("3. Find P(A or B) using P(A), P(B), and P(A and B)")
 print("***Enter only the number of your choice***")
 choice = input()
 
 #use if statements to determine which type of equation needs to be solved
-if(choice is 1):
+#Chebyshev's inequality equation
+if choice is 1:
     #ask the user for the mean
     print("\nEnter the mean")
     mean = float(input())
@@ -34,6 +40,26 @@ if(choice is 1):
     upperBound = mean + (k * standardDeviation)
 
     #print percent, lowerbound, and upperbound values
-    print("\nThe percentage is ", percent)
-    print("The lower bound is ", lowerBound)
-    print("The upper bound is ", upperBound)
+    print("\nThe percentage is " + str(percent))
+    print("The lower bound is " + str(lowerBound))
+    print("The upper bound is " + str(upperBound))
+
+#Find P(A or B)
+if choice is 3:
+    #ask for the values of P(A) and P(B)
+    a = float(input("Enter the value of A\n"))
+    b = float(input("Enter the value of B\n"))
+    
+    #ask the user if P(A and B) is mutually exclusive
+    exclusive = input("Is P(A and B) mutually exclusive?\n")
+    #if P(A and B) is mutually exlusive add the values of P(A) and P(B)
+    if exclusive == 'yes':
+        aOb = a + b
+    #if P(A and B) is not mutually exlusive, ask the user for the value of P(A and B)
+    #and add P(A) and P(B) and subtract P(A and B)
+    elif exclusive == 'no':
+        print("Enter P(A and B)")
+        aNb = float(input())
+        aOb = a + b - aNb
+    #print the value of P(A orB)
+    print("P(A or B) is equal to " + str(aOb))
